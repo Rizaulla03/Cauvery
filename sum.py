5ed59fdcfb9d6cc246140b7f0639c4884b735014
rizaulla03 <rizaulla856@gmail.com>
2023-06-01 09:55:28 +0530

    copmmit

---
+++ sum.py
@@ -0,0 +1,14 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+
+sum=a+b
+print("sum of a and b",sum)
+
+
+s1=input("enter first string") 
+s2=input("enter second string")
+
+sum=s1+s2
+print ("Sum of 2 string",sum)
+
+    
